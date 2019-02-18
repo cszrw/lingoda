4636a1b3beed7707d6327b58197af4cb3f4eebcf
rob.wilson <rob.wilson@xml-solutions.com>
2019-02-18 15:30:46 +0100

    Added a unit test to spec the structure of the Contact entity

---
+++ api/tests/Entity/ContactTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Entity\Contact;
+use PHPUnit\Framework\TestCase;
+
+class ContactTest extends TestCase
+{
+    /**
+     * Test that we have at least a plain class of the correct name 
+     * and the correct get set methods
+     */
+    public function testExists()
+    {
+        $TEST_EMAIL = "test@testdomain.test";
+        $TEST_SHORT_MESSAGE = "This is a short message";
+
+        /**
+         * Test that we have at least a plain class of the correct name 
+         * and with the correct get set methods
+         */
+        $contact = new Contact();
+        $this->assertClassHasAttribute("id", Contact::class);
+
+        $this->assertClassHasAttribute("email", Contact::class);
+        $contact->setEmail($TEST_EMAIL);
+        $contact->setMessage($TEST_SHORT_MESSAGE);
+
+        $this->assertEquals($contact->getId(),"");
+        $this->assertEquals($contact->getMessage(), $TEST_SHORT_MESSAGE);
+        $this->assertEquals($contact->getMessage(), $TEST_SHORT_MESSAGE);
+    }
+}
